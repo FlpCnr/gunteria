1f3fa0a044aeb58e2925c5db77a31bb22f81e818
Daniel Santana Rocha <danielsantanarocha@gmail.com>
2018-09-28 11:01:43 -0300

    Changed Game component location for better organization.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LaunchScreen from '@/components/LaunchScreen'
-import Game from '@/components/Game'
+import Game from '@/components/game/Game'
 
 Vue.use(Router)
 
